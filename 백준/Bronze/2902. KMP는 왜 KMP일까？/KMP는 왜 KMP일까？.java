7f8912351b8cbe496adcb2b853f83b3f25f86cca
Choi-DongJin <36064264+cdj9412@users.noreply.github.com>
2024-06-01 11:03:28 +0900

    [Bronze II] Title: KMP는 왜 KMP일까?, Time: 136 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String longFormName = br.readLine();
+        br.close();
+
+        // 제한 조건
+        if (longFormName.isEmpty() || longFormName.length() > 100) {
+            System.out.println("잘못된 입력");
+            return ;
+        }
+
+        StringBuilder shortFormName = new StringBuilder();
+        for(String lastName : longFormName.split("-"))
+            shortFormName.append(lastName.charAt(0));
+
+        // BufferedWriter 는 자동개행 기능이 없어 자동개행 처리 해줘야 함.
+        bw.write(shortFormName.toString() + "\n");
+
+        // BufferedWriter 닫기
+        bw.flush(); // 남아있던 데이터 모두 출력
+        bw.close();
+    }
+}
