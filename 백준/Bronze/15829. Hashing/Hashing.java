de70ffb865125e384ebccbf432751250d7dfd777
Choi-DongJin <36064264+cdj9412@users.noreply.github.com>
2024-06-06 09:26:44 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14292 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+     static final int r = 31;
+     static final long M = 1234567891;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // a = 97
+        int L = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+        // M 이 10억 이상이라 long 사용
+        long result = 0;
+        for (int i = 0; i < L; i++) {
+            result += (long) ((S.charAt(i)-96) * Math.pow(r, i));
+        }
+        System.out.println(result % M);
+    }
+}
